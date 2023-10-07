557a9e72cd6f56b5beda91c1a36b1fd09e7fc620
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-10-07 14:31:40 -0400

    Added hero panel

--- src/components/HeroPanel.jsx
+++ src/components/HeroPanel.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 function HeroPanel() {
   return (
-    <div>HeroPanel</div>
-  )
+    <div class="hero-container row text-center">
+      <div class="col-md-3 heroes shadow-lg mb-4">
+        <img class="hero-image" alt="Portrait of ${name}" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" />
+        <h3 class="hero-name">Hero Name</h3>
+      </div>`
+    </div>
+    )
 }
 
 export default HeroPanel
